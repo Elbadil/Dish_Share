ba79adc114dfaaa8e99b12c1382c390124279d13
Elbadil <elbaliadil@gmail.com>
2023-11-17 19:06:24 +0100

    ignored files

--- .ds_db.sql
+++
@@ -1,5 +0,0 @@
--- MySQL script that prepares a MySQL server for the project
-CREATE DATABASE IF NOT EXISTS dish_share_db;
-CREATE USER IF NOT EXISTS 'ds_user'@'localhost' IDENTIFIED BY 'adelelb15';
-GRANT ALL PRIVILEGES ON dish_share_db.* TO 'ds_user'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'ds_user'@'localhost';
